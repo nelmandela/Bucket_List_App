b109d818bbe801267c228b02b223959cd8efd4f0
Nelson Mandela <nelsonmandela784@gmail.com>
2017-06-23 07:46:11 +0300

    implemented linking bucketlist to user

--- classes/user.py
+++ classes/user.py
@@ -1,14 +1,21 @@
+from classes.bucketlist import BucketList
+
 class User():
+    users = []
     def __init__(self, name, email, password):
         self.name = name
         self.email = email
         self.password = password
-        #temporary to display bucketlists
-        self.bucketlists = ["Sky Diving", "New Language", "Bungee Jumping"]
-        print(self)
+        self.bucketlists = list()
 
     def login(self, password):
         if self.password == password:
             return True
         else:
             return False
+
+    def create_bucketlist(self, name):
+        new_bucketlist = BucketList(name)
+        self.bucketlists.append(new_bucketlist)
+        return 
+
